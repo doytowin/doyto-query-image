35c98083e792ddd40f9c4e9360df75f0c9ab58d3
Forb Yuan <f0rb@163.com>
2023-06-22 22:32:20 +0800

    update build script for mysql

--- mysql8/privileges.sql
+++ mysql8/privileges.sql
@@ -1,10 +1,4 @@
-
-create database `doytowin` default character set utf8 collate utf8_general_ci;
-
-create user 'doytowin'@'host' identified by 'doytowin';
-
-grant all privileges on doytowin.* to 'doytowin'@'%' with grant option;
-
+create database `doytowin`;
+create user 'doytowin'@'%' identified by 'doytowin';
+grant all on doytowin.* to 'doytowin'@'%' with grant option;
 flush privileges;
-
-
